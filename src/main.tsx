cade22f30d2c99f1fe1d975ac43e95cfa07db127
richfjiq <rfjiq1986@gmail.com>
2023-06-09 19:27:54 -0600

    project init and context set up

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MapsApp } from './MapsApp.tsx';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <MapsApp />
+  </React.StrictMode>
+);
